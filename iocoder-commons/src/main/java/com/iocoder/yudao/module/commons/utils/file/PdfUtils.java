06db7c831d267a28939909d0604a8a69fa2f5210
wztongkai <2580211264@qq.com>
2022-07-06 19:08:25 +0800

    pref(*) pdf 添加水印

--- iocoder-commons/src/main/java/com/iocoder/yudao/module/commons/utils/file/PdfUtils.java
+++ iocoder-commons/src/main/java/com/iocoder/yudao/module/commons/utils/file/PdfUtils.java
@@ -2,10 +2,15 @@ package com.iocoder.yudao.module.commons.utils.file;
 
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.*;
+import com.itextpdf.text.pdf.parser.ImageRenderInfo;
+import com.itextpdf.text.pdf.parser.PdfReaderContentParser;
+import com.itextpdf.text.pdf.parser.RenderListener;
+import com.itextpdf.text.pdf.parser.TextRenderInfo;
 import com.itextpdf.tool.xml.XMLWorkerFontProvider;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
 import lombok.extern.slf4j.Slf4j;
 import net.coobird.thumbnailator.Thumbnails;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.PDFRenderer;
 
@@ -13,6 +18,8 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.nio.charset.Charset;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.List;
 
@@ -221,6 +228,242 @@ public class PdfUtils {
         document.close();
         htmlFileStream.close();
         pdfFileStream.close();
+    }
+
+    /**
+     *
+     * 给pdf 添加图片水印
+     * @param inSource     pdf路径
+     * @param outSource    添加图片后的pdf路径
+     * @param sealPNG      图片路径
+     * @param keyWord      关键字
+     * @param args         日期坐标
+     * args:{
+     * int yearX
+     * int yearY
+     * int monthX
+     * int monthY
+     * int dayX
+     * int dayY
+     * }
+     */
+    public static void pdfAddImages(String inSource, String outSource, String sealPNG, String keyWord, int toAddPageNum, Map<String, Object> args) {
+        try {
+            // 读取pdf文档信息
+            Map<String, Object> pdfMsg = getPdfMsg(inSource);
+            //获取文件宽度
+            float startAddress = Float.parseFloat(pdfMsg.get("width").toString());
+            PdfReader reader = new PdfReader(new FileInputStream(inSource));
+            PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(outSource));
+
+            //利用关键字查找文件中的内容
+            List<String> keyWordsList = new ArrayList<String>();
+            keyWordsList.add(keyWord);
+            //查找关键字位置
+            List<List<float[]>> arrLists = findKeywords(keyWordsList, reader);
+            int pageNum = Integer.parseInt(pdfMsg.get("pageSize").toString());
+            //插入水印
+            Image img = Image.getInstance(sealPNG);
+            //设置尺寸
+            img.scaleAbsolute(100, 50);// 直接设定显示尺寸
+            img.setBackgroundColor(new BaseColor(0));
+            //如果没有找到关键字，走默认位置
+            if (CollectionUtils.isEmpty(arrLists)) {
+                //默认盖章位置
+                img.setAbsolutePosition(startAddress - 188, 70);
+                PdfContentByte overContent = stamper.getOverContent(pageNum);
+                overContent.addImage(img);
+                stamper.close();
+                reader.close();
+                return;
+            }
+//            if (!arrLists.get(0).isEmpty()) {
+//                // 关键字所在的页码数
+//                if (Objects.equals(keyWord, "单位领导签字")) {
+//                    img.setAbsolutePosition(arrLists.get(0).get(0)[0] - 370,
+//                            arrLists.get(0).get(0)[1] - 70);
+//                }
+//                PdfContentByte under = stamper.getOverContent(toAddPageNum);
+//                under.addImage(img);
+//                //添加文字水印
+//                //doTextHide(under);
+//                //图片水印
+//                //doImgHide(under, sealPNG);
+//            }
+            PdfContentByte under = stamper.getOverContent(pageNum);
+            //int yearX = 549;
+            int yearX = (Integer)args.get("yearX");
+            //int yearY = 637;
+            int yearY = (Integer)args.get("yearY");
+            //int monthX = 503;
+            int monthX = (Integer)args.get("monthX");
+            //int monthY = 637;
+            int monthY = (Integer)args.get("monthY");
+            //int dayX = 470;
+            int dayX = (Integer)args.get("dayX");
+            //int dayY = 637;
+            int dayY = (Integer)args.get("dayY");
+            String year = DateTimeFormatter.ofPattern("yyyy").format(LocalDate.now());
+            String month = DateTimeFormatter.ofPattern("MM").format(LocalDate.now());
+            String day = DateTimeFormatter.ofPattern("dd").format(LocalDate.now());
+            //添加文字水印
+            doTextHide2(under, startAddress, yearX, yearY, year,14);
+            doTextHide2(under, startAddress, monthX, monthY, month, 14);
+            doTextHide2(under, startAddress, dayX, dayY, day, 14);
+
+            stamper.close();
+            reader.close();
+        } catch (DocumentException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * pdf 添加文字水印
+     * @param under
+     * @param startAddress
+     * @param x
+     * @param y
+     * @param date
+     * @param fontSize
+     * @throws DocumentException
+     * @throws IOException
+     */
+    public static void doTextHide2(PdfContentByte under, float startAddress, int x, int y, String date, int fontSize) throws DocumentException,
+            IOException {
+        PdfGState gState = new PdfGState();
+        under.beginText();
+        BaseFont bf = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
+        under.setFontAndSize(bf, fontSize);
+        under.setTextMatrix(startAddress - x, y);
+        gState.setFillOpacity(1f);
+        under.setGState(gState);
+        //设置字体颜色
+        under.setColorFill(BaseColor.BLACK);
+        under.showText(date);
+        under.endText();
+    }
+
+    /**
+     * 获取pdf文档信息
+     *
+     * @param pdfPath pdf 文件路径
+     * @return 文档信息
+     */
+    public static Map<String, Object> getPdfMsg(String pdfPath) {
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        try {
+            // 获取PDF共有几页
+            PdfReader pdfReader = new PdfReader(new FileInputStream(pdfPath));
+            int pages = pdfReader.getNumberOfPages();
+            map.put("pageSize", pages);
+            // 获取PDF 的宽高
+            PdfReader pdfreader = new PdfReader(pdfPath);
+            Document document = new Document(pdfreader.getPageSize(pages));
+            float widths = document.getPageSize().getWidth();
+            // 获取页面高度
+            float heights = document.getPageSize().getHeight();
+            map.put("width", widths);
+            map.put("height", heights);
+            document.close();
+            pdfreader.close();
+            pdfReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    /**
+     * 获取关键字位置(找第一个，其他的忽略)
+     *
+     * @param keyWords  关键字
+     * @param pdfReader pdfReader
+     * @return 关键字位置
+     */
+    public static List<List<float[]>> findKeywords(List<String> keyWords, PdfReader pdfReader) {
+        if (keyWords.isEmpty()) {
+            return null;
+        }
+        int pageNum = pdfReader.getNumberOfPages();
+        final List<List<float[]>> arrayLists = new ArrayList<List<float[]>>(keyWords.size());
+        for (int k = 0; k < keyWords.size(); k++) {
+            List<float[]> positions = new ArrayList<float[]>();
+            arrayLists.add(positions);
+        }
+        PdfReaderContentParser pdfReaderContentParser = new PdfReaderContentParser(pdfReader);
+        try {
+            for (int i = 1; i <= pageNum; i++) {
+                final int finalI = i;
+                pdfReaderContentParser.processContent(i, new RenderListener() {
+                    public String pdfsb = "";
+                    private float yy = -1f;
+
+                    @Override
+                    public void renderText(TextRenderInfo textRenderInfo) {
+                        String text = textRenderInfo.getText();
+                        com.itextpdf.awt.geom.Rectangle2D.Float boundingRectange = textRenderInfo.getBaseline().getBoundingRectange();
+                        if (yy == -1f) {
+                            yy = boundingRectange.y;
+                        }
+                        if (yy != boundingRectange.y) {
+                            yy = boundingRectange.y;
+                        }
+                        pdfsb += text;
+                        if (pdfsb.length() > 0) {
+                            for (int j = 0; j < keyWords.size(); j++) {
+                                List<String> key_words = parseList(keyWords.get(j), ",");
+                                //假如配置了多个关键字，找到一个就跑
+                                for (final String key_word : key_words) {
+                                    if (pdfsb.length() > 0 && pdfsb.toString().contains(key_word)) {
+                                        float[] resus = new float[3];
+                                        resus[0] = boundingRectange.x + boundingRectange.width * (key_word.length() - 1);
+                                        resus[1] = boundingRectange.y;
+                                        resus[2] = finalI;
+                                        arrayLists.get(j).add(resus);
+                                        pdfsb = "";
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void renderImage(ImageRenderInfo arg0) {
+                    }
+
+                    @Override
+                    public void endTextBlock() {
+                    }
+
+                    @Override
+                    public void beginTextBlock() {
+                    }
+                });
+            }
+        } catch (Exception e) {
+            log.error("出现异常" + e.getMessage());
+        }
+        return arrayLists;
+    }
+
+    public static List<String> parseList(String source, String regex) {
+        if (source.isEmpty()) {
+            return null;
+        }
+        List<String> strList = new ArrayList<String>();
+        if (regex.isEmpty()) {
+            strList.add(source);
+        } else {
+            String[] strArr = source.split(regex);
+            for (String str : strArr) {
+                if (!str.isEmpty()) {
+                    strList.add(str);
+                }
+            }
+        }
+        return strList;
     }
 
 
