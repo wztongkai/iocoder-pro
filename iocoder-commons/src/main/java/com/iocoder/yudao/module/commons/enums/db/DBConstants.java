7fd964604ce4f4c2349484dc0783f35008a77bfd
wztongkai <2580211264@qq.com>
2022-07-07 16:17:30 +0800

    pref(*)完善多数据源配置

---
+++ iocoder-commons/src/main/java/com/iocoder/yudao/module/commons/enums/db/DBConstants.java
@@ -0,0 +1,16 @@
+package com.iocoder.yudao.module.commons.enums.db;
+
+/**
+ * 数据源枚举类
+ */
+public class DBConstants {
+    /**
+     * 数据源分组 - 主库
+     */
+    public static final String DATASOURCE_MASTER = "master";
+
+    /**
+     * 数据源分组 - 从库
+     */
+    public static final String DATASOURCE_SLAVE = "slave";
+}
