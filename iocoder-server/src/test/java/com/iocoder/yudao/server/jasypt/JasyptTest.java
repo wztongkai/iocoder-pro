a1596610daae51e5fad814b49057b74a6d9cfbe5
wztongkai <2580211264@qq.com>
2022-10-25 11:15:35 +0800

    prod(*):去除jasypt加密秘钥，将秘钥作为启动参数带入

--- iocoder-server/src/test/java/com/iocoder/yudao/server/jasypt/JasyptTest.java
+++ iocoder-server/src/test/java/com/iocoder/yudao/server/jasypt/JasyptTest.java
@@ -1,33 +1,37 @@
-package com.iocoder.yudao.server.jasypt;
-
-
-import com.iocoder.yudao.server.IoCoderApplication;
-import com.ulisesbocchio.jasyptspringboot.encryptor.DefaultLazyEncryptor;
-import org.jasypt.encryption.StringEncryptor;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.StandardEnvironment;
-import org.springframework.test.context.junit4.SpringRunner;
-
-/**
- * @author wu kai
- * @date 2022/6/24
- */
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = IoCoderApplication.class)
-public class JasyptTest {
-
-    @Test
-    public void test() {
-        // 对应配置文件中配置的加密密钥
-        System.setProperty("jasypt.encryptor.password", "strangegirl");
-        StringEncryptor stringEncryptor = new DefaultLazyEncryptor(new StandardEnvironment());
-        System.out.println("用户名加密： " + stringEncryptor.encrypt("root"));
-        System.out.println("用户名解密： " + stringEncryptor.decrypt(stringEncryptor.encrypt("root")));
-
-        System.out.println("密码加密： " + stringEncryptor.encrypt("123456"));
-        System.out.println("密码解密： " + stringEncryptor.decrypt(stringEncryptor.encrypt("123456")));
-
-    }
-}
+//package com.iocoder.yudao.server.jasypt;
+//
+//
+//import com.iocoder.yudao.server.IoCoderApplication;
+//import org.jasypt.util.text.BasicTextEncryptor;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+///**
+// * @author wu kai
+// * @date 2022/6/24
+// */
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = IoCoderApplication.class)
+//public class JasyptTest {
+//
+//    @Test
+//    public void test() {
+//        // 对应配置文件中配置的加密密钥
+////        System.setProperty("jasypt.encryptor.password", "Bt%XJ^n1j8mz");
+////        StringEncryptor stringEncryptor = new DefaultLazyEncryptor(new StandardEnvironment());
+////        System.out.println("用户名加密： " + stringEncryptor.encrypt("root"));
+////        System.out.println("用户名解密： " + stringEncryptor.decrypt(stringEncryptor.encrypt("root")));
+////
+////        System.out.println("密码加密： " + stringEncryptor.encrypt("123456"));
+////        System.out.println("密码解密： " + stringEncryptor.decrypt(stringEncryptor.encrypt("123456")));
+//
+//        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+//        textEncryptor.setPassword("WkZuiShuai");
+//        String username = textEncryptor.encrypt("root");
+//        String password = textEncryptor.encrypt("123456");
+//        System.out.println("username: " + username);
+//        System.out.println("password:" + password);
+//    }
+//}
