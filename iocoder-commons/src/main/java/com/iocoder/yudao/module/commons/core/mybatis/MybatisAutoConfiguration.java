5116103c4b39003b6257eab435b635e106acc672
wztongkai <2580211264@qq.com>
2022-10-25 14:28:57 +0800

    fix(*):修改系统日志打印问题（解决日志文件中输出的日志中带有大量ESC的问题）

--- iocoder-commons/src/main/java/com/iocoder/yudao/module/commons/core/mybatis/MybatisAutoConfiguration.java
+++ iocoder-commons/src/main/java/com/iocoder/yudao/module/commons/core/mybatis/MybatisAutoConfiguration.java
@@ -3,7 +3,6 @@ package com.iocoder.yudao.module.commons.core.mybatis;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
  * @Date: 2022/6/3 14:57
  */
 @Configuration
-@MapperScan("com.wkboot.mapper")
 public class MybatisAutoConfiguration {
 
     /**
